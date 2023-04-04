cea25d95a20bf173d85f651779b5876341966b7f
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-04-05 00:54:15 +0800

    Modify program

--- CODE-Interpreter/Program.cs
+++ CODE-Interpreter/Program.cs
@@ -1,4 +1,6 @@
 namespace CODE_Interpreter;
+using Antlr4.Runtime;
+using CODE_Interpreter.Content;
 
 static class Program
 {
@@ -10,7 +12,23 @@ static class Program
     {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        // ApplicationConfiguration.Initialize();
+        // Application.Run(new Form1());
+
+
+        var filename = "Content\\input.txt";
+
+        var code = File.ReadAllText(filename);
+        Console.WriteLine(code);
+
+        var inputStream = new AntlrInputStream(code);
+        var codeLexer = new CodeLexer(inputStream);
+        var commonTokenStream = new CommonTokenStream(codeLexer);
+        var codeParser = new CodeParser(commonTokenStream);
+
+        var codeContext = codeParser.program();
+        var visitor = new CodeVisitor();
+
+        visitor.Visit(codeContext);
     }    
 }
