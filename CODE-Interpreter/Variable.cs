f124479090f18fa1084b1cc54fcd32757085cfaf
Johniel Comanda <johniel.comanda@cit.edu>
2023-04-09 12:22:41 +0800

    Create model for variable

---
+++ CODE-Interpreter/Variable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODE_Interpreter
+{
+    internal class Variable
+    {
+        public string? DataType { get; set; }
+        public string? Identifier { get; set; }
+        public object? Value { get; set; } 
+    }
+}
